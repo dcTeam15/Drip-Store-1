22a33ff2a784eda013264ac1d4e1ffce3af759eb
kevin <kevindiegodasilvasousa@gmail.com>
2023-06-14 11:09:29 -0300

    feat: create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import './App.css'
-import Footer from './components/Footer/Footer'
-const App = () => {
- 
+import { createContext, useEffect } from "react";
+import { Ways } from "./routes";
+import { useState } from "react";
+
+export const LoginContext = createContext(null);
+
+export const App = () => {
+  const [logado, setLogado] = useState(false);
+
+  useEffect(() => {
+    const logado = JSON.parse(localStorage.getItem('logado')) || false;
+    setLogado(logado)
+  }, [])
 
   return (
     <>
-    
-        
+      <LoginContext.Provider value={{logado, setLogado}} >
+        <Ways />
+      </LoginContext.Provider>
     </>
-  )
-}
-
-export default App;
+  );
+};
