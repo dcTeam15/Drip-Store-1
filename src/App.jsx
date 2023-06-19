b1289150bcdeebb7055d134759c691e8f469013e
gleidsonteixeira <gleidsondeveloper@gmail.com>
2023-06-19 08:31:31 -0300

    inicio

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,6 @@ const App = () => {
 
   return (
     <>
-<<<<<<< HEAD
       <AuthContext.Provider value={{isLoggeg, setIsLoggeg}}>
         <Ways />
       </AuthContext.Provider>
@@ -28,11 +27,3 @@ const App = () => {
 }
 
 export default App
-=======
-      <LoginContext.Provider value={{logado, setLogado}} >
-        <Ways />
-      </LoginContext.Provider>
-    </>
-  );
-};
->>>>>>> 22a33ff2a784eda013264ac1d4e1ffce3af759eb
